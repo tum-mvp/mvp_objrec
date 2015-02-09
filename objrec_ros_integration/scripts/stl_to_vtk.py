dcc98ed219ddf0956f98b2ec0b49e096a52b1f1f
Jonathan Bohren <jbo@jhu.edu>
2015-02-09 14:09:07 -0500

    adding python-based stl/vtk converters

---
+++ objrec_ros_integration/scripts/stl_to_vtk.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+import vtk
+
+if __name__ == '__main__':
+
+    in_filename = sys.argv[1]
+    out_filename = sys.argv[2]
+
+    # Read the vtk file
+    reader = vtk.vtkSTLReader()
+    reader.SetFileName(in_filename)
+    reader.Update()
+
+    # Write the stl file to disk
+    writer = vtk.vtkPolyDataWriter()
+    writer.SetFileName(out_filename)
+    writer.SetInputConnection(reader.GetOutputPort())
+    writer.Write()
